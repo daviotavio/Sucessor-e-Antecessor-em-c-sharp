11889f87e1db13ebd13cb10afa65796a42afc17f
Davi Otavio <85647007+daviotavio@users.noreply.github.com>
2021-09-04 20:23:54 -0300

    Add files via upload

---
+++ sucessor e antecessor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace sucessor_e_antecessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int sucessor;
+            int antecessor;
+
+            Console.Write("Digite um número: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            antecessor = numero -1;
+            sucessor = numero +1;
+
+            Console.Write("O número " +numero + " tem como antecessor " + antecessor + " e como sucessor " +sucessor +".");
+        }
+    }
+}
